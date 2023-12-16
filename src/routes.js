87ac170babacde89029acf9426af27f70788f9d9
Matheus <matheustorresdev@gmail.com>
2023-12-16 20:03:40 -0300

    add patch method

--- src/routes.js
+++ src/routes.js
@@ -1,68 +1,87 @@
-import { Database } from './database.js'
-import { randomUUID } from 'node:crypto'
-import { buildRoutePath } from './utils/build-route-path.js'
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
-const database = new Database()
+const database = new Database();
 
-export const routes = [{
-    method: 'GET',
-    path:buildRoutePath('/task'),
-    handler: (req, res)=> {
-        const {search} = req.query
-        const users = database.select('tasks',search ?  {
-            title: search,
-            description: search
-        }: null)
+export const routes = [
+  {
+    method: "GET",
+    path: buildRoutePath("/task"),
+    handler: (req, res) => {
+      const { search } = req.query;
+      const users = database.select(
+        "tasks",
+        search
+          ? {
+              title: search,
+              description: search,
+            }
+          : null
+      );
 
-        return res.end(JSON.stringify(users))
-    }
-},
-{
-    method: 'POST',
-    path:buildRoutePath('/task'),
-    handler: (req, res)=> {
-        const {name, email} = req.body
-        const user = {
-            id: randomUUID(),
-            title,
-            description,
-            completed_at: null,
-            created_at: new Date().toISOString(),
-            updated_at: new Date().toISOString()
-        }
+      return res.end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/task"),
+    handler: (req, res) => {
+      const { name, email } = req.body;
+      const user = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      };
 
-        database.insert('tasks', user)
+      database.insert("tasks", user);
 
-        return res.writeHead(201).end()
-    }
-},
-{
-    method: 'DELETE',
-    path:buildRoutePath('/users/:id'),
-    handler:(req,res)=> {
-        const { id } = req.params
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/task/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
 
-        database.delete('users', id)
+      database.delete("tasks", id);
 
-        return res.writeHead(204).end()
-        }
-},
-{
-    method: 'PUT',
-    path:buildRoutePath('/task/:id'),
-    handler:(req,res)=> {
-        const { id } = req.params
-        const { title, description, completed_at, created_at, updated_at} = req.body
-        database.update('tasks', id, {
-            id,
-            title,
-            description,
-            completed_at,
-            created_at,
-            updated_at
-        })
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/task/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description, completed_at, created_at, updated_at } =
+        req.body;
+      database.update("tasks", id, {
+        id,
+        title,
+        description,
+        completed_at,
+        created_at,
+        updated_at,
+      });
 
-        return res.writeHead(204).end()
-        }
-}
-]   
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "PATCH",
+    path: buildRoutePath("/task/:id/complete"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      database.updatePatch("tasks", id, {
+        completed_at: new Date().toISOString(),
+      });
+
+      return res.writeHead(204).end();
+    },
+  },
+];
